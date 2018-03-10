976db834b24097890e509ca98fabca9be029fb16
Chris Underwood <cunderwoodmn@gmail.com>
2018-03-09 20:03:35 -0800

    removed unnecessary className entries

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,8 +4,8 @@ class Home extends Component {
   render() {
     return (
       <div className="App">
-      
-        <p className="App-intro">
+
+        <p >
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
